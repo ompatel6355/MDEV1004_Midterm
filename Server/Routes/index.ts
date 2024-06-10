3f90aa13c92225cbd0e8cea7083fcdc1a69a7625
ompatel6355 <74576844+ompatel6355@users.noreply.github.com>
2024-06-10 13:52:13 -0400

    Added project file

---
+++ Server/Routes/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+const router = express.Router();
+
+import { DisplayMovieList, DisplayMovieById, AddMovie, UpdateMovie, DeleteMovie } from '../Controllers/movie';
+
+/* List of Routes (endpoints) */
+
+/* GET Movie List. */
+router.get('/', (req, res, next) => {  DisplayMovieList(req, res, next); });
+
+/* GET Movie by ID. */
+router.get('/:id', (req, res, next) => {  DisplayMovieById(req, res, next); });
+
+/* Add Movie */
+router.post('/add', (req, res, next) => {  AddMovie(req, res, next); });
+
+/* Update Movie */
+router.put('/update/:id', (req, res, next) => {  UpdateMovie(req, res, next); });
+
+/* Delete Movie */
+router.delete('/delete/:id', (req, res, next) => {  DeleteMovie(req, res, next); });
+
+export default router;
